e0f3336176afdbd35f326e5fc09a5234852d7539
Keisuke Iwamura <i.ksk0601@gmail.com>
2022-10-18 17:36:51 +0900

    feat: add lower script

---
+++ scripts/lint_address.py
@@ -0,0 +1,11 @@
+from os import path
+from token_original_id.utils import load_csv, save_csv, lint_address
+
+def lint_csv_address():
+    csv_file = path.join(path.dirname(__file__), "../token_original_id/data.csv")
+    lines = load_csv(csv_file)
+    updated_lines = lint_address(lines)
+    save_csv(csv_file, "\n".join(updated_lines) + "\n")
+
+if __name__ == '__main__':
+    lint_csv_address()
